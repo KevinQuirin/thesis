1adb25341299a5327bcd66b58bd60aec87a5e94e
KevinQuirin <kevin.quirin@gmail.com>
2016-02-29 15:14:57 +0100

    Script git hook

--- pushlatex.sh
+++ pushlatex.sh
@@ -2,8 +2,6 @@
 
 latexmk -pdf main.tex
 pdflatex main.tex | grep -i -f errors.grep > errors.log
-cp main.pdf ~/Code/Tex/thesis_gh_pages/thesis/thesis.pdf
-cp errors.log ~/Code/Tex/thesis_gh_pages/thesis/errors.log
 
 git checkout gh-pages
 git commit -a -m "Autocommit"
