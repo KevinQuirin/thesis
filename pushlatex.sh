f669649ebdbdc815dcb2166e874e14c3b5d49530
KevinQuirin <kevin.quirin@gmail.com>
2016-02-29 15:36:05 +0100

    foo

--- pushlatex.sh
+++ pushlatex.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-
-latexmk -pdf main.tex
-pdflatex main.tex | grep -i -f errors.grep > errors.log
-
-git checkout gh-pages
-git commit -a -m "Autocommit"
-git push
-git checkout master
+branch=master
+if [ `git rev-parse - abbrev-ref HEAD` == "$branch" ]; then
+    latexmk -pdf main.tex
+    pdflatex main.tex | grep -i -f errors.grep > errors.log
+    
+    git checkout gh-pages
+    git rm main.pdf
+    git rm errors.log
+    git checkout $branch -- main.pdf
+    git checkout $branch -- errors.log
+    git add -A
+    git commit -a -m "Autocommit $(date +"%d.%m.%Y %H:%M:%S")"
+    git push
+    git checkout master
+fi
 
 exit 0
