1dd9dd3456f2e4847223b88f02edd5d5dddf7acc
KevinQuirin <kevin.quirin@gmail.com>
2016-05-11 16:59:46 +0200

    foo

--- pushlatex.sh
+++ pushlatex.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 branch=master
 if [ `git rev-parse --abbrev-ref HEAD` == "$branch" ]; then
-    latexmk -pdf main.tex
-    pdflatex main.tex | grep -i -f errors.grep > errors.txt
-    cp main.pdf ../thesis-gh-pages/thesis/main.pdf
-    cp errors.txt ../thesis-gh-pages/thesis/errors.txt
-    cd ../thesis-gh-pages/thesis/
-    sed -i.old -e 's#[0-9][0-9]*/[0-9][0-9]*/[0-9][0-9][0-9][0-9][ ]@[ ][0-9][0-9]*:[0-9][0-9]*#'$(date +%d/%m/%Y)' @ '$(date +%H:%M)'#g' index.html
-    rm index.html.old
-    git commit main.pdf errors.txt index.html -m "Autocommit $(date +"%d.%m.%Y %H:%M:%S")"
-    git push
-    cd ../../thesis
+    latexmk -pdf main.tex #compile the project if needed
+    pdflatex main.tex | grep -i -f errors.grep > errors.txt #compile to catch the errors
+    cp main.pdf ../thesis-gh-pages/thesis/main.pdf #copy the generated pdf to the gh-pages folder
+    cp errors.txt ../thesis-gh-pages/thesis/errors.txt #copy the list of errors to the gh-pages folder
+    cd ../thesis-gh-pages/thesis/ #go to the gh-pages folder
+    sed -i.old -e 's#[0-9][0-9]*/[0-9][0-9]*/[0-9][0-9][0-9][0-9][ ]@[ ][0-9][0-9]*:[0-9][0-9]*#'$(date +%d/%m/%Y)' @ '$(date +%H:%M)'#g' index.html #change the date of update in index.html
+    rm index.html.old #remove the old one FIXME
+    git commit main.pdf errors.txt index.html -m "Autocommit $(date +"%d.%m.%Y %H:%M:%S")" #commit the changes
+    git push #put the changes
+    cd ../../thesis #go back to the .tex's folder
 fi
 
 exit 0
