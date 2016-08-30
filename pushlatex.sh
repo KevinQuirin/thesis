f54a56afa07f5f003a9c65c88f59cb25c0144c98
KevinQuirin <kevin.quirin@gmail.com>
2016-08-30 18:15:44 +0200

    oo

--- pushlatex.sh
+++ pushlatex.sh
@@ -3,9 +3,9 @@ branch=master
 if [ `git rev-parse --abbrev-ref HEAD` == "$branch" ]; then
     latexmk -pdf main.tex #compile the project if needed
     pdflatex --file-line-error main.tex | grep -i -f errors.grep > errors.txt #compile to catch the errors
-    cp main.pdf ../thesis_page/thesis/main.pdf #copy the generated pdf to the gh-pages folder
-    cp errors.txt ../thesis_page/thesis/errors.txt #copy the list of errors to the gh-pages folder
-    cd ../thesis_page/thesis/ #go to the gh-pages folder
+    cp main.pdf ../thesis-gh-pages/thesis/main.pdf #copy the generated pdf to the gh-pages folder
+    cp errors.txt ../thesis-gh-pages/thesis/errors.txt #copy the list of errors to the gh-pages folder
+    cd ../thesis-gh-pages/thesis/ #go to the gh-pages folder
     sed -i.old -e 's#[0-9][0-9]*/[0-9][0-9]*/[0-9][0-9][0-9][0-9][ ]@[ ][0-9][0-9]*:[0-9][0-9]*#'$(date +%d/%m/%Y)' @ '$(date +%H:%M)'#g' index.html #change the date of update in index.html
     rm index.html.old #remove the old one FIXME
     git commit main.pdf errors.txt index.html -m "Autocommit $(date +"%d.%m.%Y %H:%M:%S")" #commit the changes
